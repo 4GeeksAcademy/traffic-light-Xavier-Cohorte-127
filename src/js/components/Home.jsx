d892b151bc655f1c23af0bd72ba8359756d418be
alpxavi17-maker <alpxavi17@gmail.com>
2025-12-15 19:06:02 +0000

    Guardando

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,16 +5,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [color, setColor]= useState ("")
-	
+	const [color, setColor] = useState("")
+
 
 
 	return (
 		<div className="traffic-light">
-			<div className="red" onClick={()=>{setColor("red")}}></div>
-			<div className="yellow"onClick={()=>{setColor("yellow")}}>{color}</div>
-			<div className="green"onClick={()=>{setColor("green")}}></div>
-			
+			<div className={`red ${color === "red" ? "active" : ""}`} onClick={() => setColor("red")}></div>
+			<div className={`yellow ${color === "yellow" ? "active" : ""}`} onClick={() => setColor("yellow")}></div>
+			<div className={`green ${color === "green" ? "active" : ""}`} onClick={() => setColor("green")}></div>
+
 		</div>
 	);
 };
